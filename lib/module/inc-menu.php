0819cb13a199060607429812c87d5d943c8baa84
smetana228 <65881878+smetana228@users.noreply.github.com>
2023-02-03 23:36:39 +0600

    Add files via upload

---
+++ lib/module/inc-menu.php
@@ -0,0 +1,145 @@
+
+
+<div class="theme-btn-close toggle-theme">
+</div>
+<div class="settings-menu-con-outer settings-submenu">
+	<div class="settings-menu-con-wrapper toggle-darkmode settings-menu-upper">
+		<div class="settings-menu-ico-darkmode">
+		</div>
+		<div class="settings-menu-btn-button btn-darkmode">
+			<span>Темная тема - On/Off</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-transparency">
+		<div class="settings-menu-ico-transparency">
+		</div>
+		<div class="settings-menu-btn-button btn-transparency">
+			<span>Прозрачность - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-particles">
+		<div class="settings-menu-ico-particles">
+		</div>
+		<div class="settings-menu-btn-button btn-particles">
+			<span>Блестки - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-pulsate">
+		<div class="settings-menu-ico-pulsate">
+		</div>
+		<div class="settings-menu-btn-button btn-pulsate">
+			<span>Пульсация - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-divider">
+	</div>
+	<div class="settings-menu-con-wrapper toggle-waves">
+		<div class="settings-menu-ico-waves">
+		</div>
+		<div class="settings-menu-btn-button btn-waves">
+			<span>Волны - On</span>
+		</div>
+	</div>
+</div>
+<div class="menu-btn-backtotop">
+</div>
+<div class='menu-con-container'>
+	<div class="menu-con-backdrop darkmode-menu">
+	</div>
+	<div class="menu-btn-settings toggle-settings">
+	</div>
+	<div class="menu-con-outer">
+		<div class="menu-con-inner">
+			<div class="menu-con-logo">
+				<a href='/'>
+				<div class="menu-ico-logo">
+				</div>
+				</a>
+			</div>
+			<div class="mobile-menu-btn-open toggle-mobilemenu">
+			</div>
+			<div class="mobile-menu-btn-darkmode toggle-darkmode">
+			</div>
+			<!--
+			<a href='/reviews'>
+			<div class="menu-btn-select">
+				<span>Заведения</span>
+			</div>
+			</a>
+			<div class="menu-con-divider">
+			</div>
+			-->
+
+			<a href='/about'>
+			<div class="menu-btn-select">
+				<span>О проекте</span>
+			</div>
+			</a>
+			<div class="menu-con-divider">
+			</div>
+			
+			<a href='https://www.instagram.com/r.v.teens/' target="_blank">
+			<div class="menu-btn-select">
+				<span>Instagram</span>
+			</div>
+			</a>
+			<div class="support-con-menu">
+				<a href="https://www.patreon.com/Nekotekina" target="_blank">
+				<div class="support-ico-menu">
+				</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class='mobile-menu-con-container popup-mobilemenu' style="display:none;">
+	<div class="menu-con-backdrop-2">
+	</div>
+	<div class='mobile-menu-con-scroll'>
+		<div class="mobile-menu-con-outter">
+			<div class="mobile-menu-con-inner" style="overflow: hidden !important;">
+				<div class='mobile-menu-tx1-group'>
+					<span>R.V. Teens</span>
+				</div>
+				<div class='mobile-menu-con-divider'>
+				</div>
+				<a href="/">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Главная</span>
+				</div>
+				</a>
+				<a href="/reviews">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Заведения</span>
+				</div>
+				</a>
+				<a href="/about">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>О проекте</span>
+				</div>
+				</a>
+				<a href='https://www.instagram.com/r.v.teens/'>
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Instagram</span>
+				</div>
+				</a>
+				
+				
+			</div>
+		</div>
+	</div>
+</div>
---
+++ lib/module/inc-video-carousel.php
@@ -0,0 +1,67 @@
+<div class="container fill">
+	<div id="myCarousel" class="carousel slide">
+		<div class="carousel-inner">
+			<!-- Carousel Slide -->
+			<div class="item active">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								
+								<a href="https://2gis.kg/bishkek/branches/70000001019366592?m=74.615439%2C42.829069%2F13.38%2Fr%2F7" target = blank_><img class='video-img-thumbnail'src="/img/videos/1.jpg"></a>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-4">
+								<div class='video-emp-block'>
+								</div>
+								<h2><a href="https://2gis.kg/bishkek/branches/70000001019366592?m=74.615439%2C42.829069%2F13.38%2Fr%2F7" target = blank_>Adriano coffe</a></h2>
+								<h3></h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Carousel Slide -->
+			<div class="item">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<a href="https://2gis.kg/bishkek/search/%D0%B3%D1%80%D0%B5%D0%BD%D0%BA%D0%B8?m=74.615439%2C42.829069%2F13.38%2Fr%2F7" target=blank_><img class='video-img-thumbnail'src="/img/videos/2.jpg"></a>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-5">
+								<div class='video-emp-block'>
+								</div>
+								<h2><a href="https://2gis.kg/bishkek/search/%D0%B3%D1%80%D0%B5%D0%BD%D0%BA%D0%B8?m=74.615439%2C42.829069%2F13.38%2Fr%2F7" target=blank_>Гренки</a></h2>
+								<h3></h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Carousel Slide -->
+			<div class="item">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<a href="https://2gis.kg/bishkek/search/%D0%B3%D1%80%D0%B5%D0%BD%D0%BA%D0%B8?m=74.615439%2C42.829069%2F13.38%2Fr%2F7" target=blank_><img class='video-img-thumbnail'src="/img/videos/3.jpg"></a>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-6">
+								<div class='video-emp-block'>
+								</div>
+								<h2><a href="https://2gis.kg/bishkek/search/%D0%B3%D1%80%D0%B5%D0%BD%D0%BA%D0%B8?m=74.615439%2C42.829069%2F13.38%2Fr%2F7" target=blank_>La Maison du voyageur</a></h2>
+								<h3></h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
